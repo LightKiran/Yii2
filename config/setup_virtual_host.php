5195d8d3f8b57423dc9e83e56b28d37347aa4fad
kiranduwal <kiranduwal@hotmail.com>
2020-02-28 14:20:40 +0545

    Setup virtual host in window 8 os

---
+++ config/setup_virtual_host.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+
+C:\xampp\apache\conf\extra edit this file httpd-vhosts.conf
+
+<VirtualHost *:80>
+    ServerAdmin webmaster@dummy-host2.example.com
+    DocumentRoot "C:/xampp/htdocs/yiitwo/basicyiicrud/web"
+    ServerName yii2basiccrud.test
+</VirtualHost>
+ 
+ 
+
+ C:\Windows\System32\drivers\etc edit this file **hosts**
+ * 
+ * 
+ * 
+ * add this at last 
+ 127.0.0.1 		    yii2basiccrud.test
+
+*/
+
